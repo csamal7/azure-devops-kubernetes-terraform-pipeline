b1c0be7759124ce18c434e2d286cd9e84f96225f
csamal7 <chinmoysamal5@gmail.com>
2024-05-29 22:59:38 +0530

    updating main.tf file

--- configuration/iaac/aws/kubernetes/main.tf
+++ configuration/iaac/aws/kubernetes/main.tf
@@ -26,7 +26,7 @@ data "aws_subnets" "subnets" {
 }
 provider "kubernetes" {
   host                   = data.aws_eks_cluster.cluster.endpoint
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.data)
+  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
   token                  = data.aws_eks_cluster_auth.cluster.token
  # version                = "~> 2.12"
 }
